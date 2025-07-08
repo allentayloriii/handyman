7bb01758c22e337feb790bcd3f29342e86e0810e
SoundtrkSamurai <furtimat3@yahoo.com>
2025-07-08 13:05:19 -0700

    feat: implement Films component and add Film interface

--- swapi-app/app/films.tsx
+++ swapi-app/app/films.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import { COLORS } from "@/constants/colors";
+import { Film } from "@/types/interfaces";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-const films = () => {
+const Films = () => {
+  const [films, setFilms] = useState<Film[]>([]);
   return (
     <View style={styles.container}>
       <Text>Films</Text>
@@ -9,8 +12,11 @@ const films = () => {
   );
 };
 
-export default films;
+export default Films;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.containerBackground,
+  },
 });

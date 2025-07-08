0cfa428e9b9ab5706c9a31bff807d4189b1d7915
SoundtrkSamurai <furtimat3@yahoo.com>
2025-07-08 13:50:15 -0700

    feat: implement useFetchFilms hook for fetching film data

--- swapi-app/app/films.tsx
+++ swapi-app/app/films.tsx
@@ -1,18 +1,8 @@
 import { COLORS } from "@/constants/colors";
-import { Film } from "@/types/interfaces";
-import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-const Films = () => {
-  const [films, setFilms] = useState<Film[]>([]);
-  return (
-    <View style={styles.container}>
-      <Text>Films</Text>
-    </View>
-  );
-};
-
-export default Films;
+import { useFetchFilms } from "@/hooks/useFetchFilms";
+import { Film } from "@/types/interfaces"; // Assuming you have a Film type defined
+import React from "react";
+import { FlatList, RefreshControl, StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -20,3 +10,36 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.containerBackground,
   },
 });
+
+const Films = () => {
+  const { films, refreshing, onRefresh } = useFetchFilms();
+
+  const renderItem = ({ item }: { item: Film }) => (
+    <Text style={{ color: "white" }}>{item.title}</Text>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={films}
+        keyExtractor={(item) => item.episode_id.toString()}
+        renderItem={renderItem}
+        // RefreshControl is used to pull to refresh the list
+        refreshControl={
+          <RefreshControl
+            onRefresh={onRefresh}
+            refreshing={refreshing}
+            tintColor={COLORS.text}
+          />
+        }
+        ListEmptyComponent={
+          <Text style={{ color: "white", textAlign: "center", marginTop: 20 }}>
+            No films available
+          </Text>
+        }
+      />
+    </View>
+  );
+};
+
+export default Films;
---
+++ swapi-app/hooks/useFetchFilms.ts
@@ -0,0 +1,37 @@
+import { Film } from '@/types/interfaces';
+import { useEffect, useState } from 'react';
+
+export function useFetchFilms() {
+  const [films, setFilms] = useState<Film[]>([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const fetchFilms = async () => {
+    setLoading(true);
+    try {
+      // Fetch films from an API or database
+      const response = await fetch("https://swapi.info/api/films");
+      const data = await response.json();
+      console.log("Fetched films:", data.results);
+      setFilms(data);
+    } catch (error) {
+      console.error("Error fetching films:", error);
+    } finally {
+      setLoading(false);
+      setRefreshing(false);
+    }
+  };
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    fetchFilms();
+  }
+  
+
+  useEffect(() => {
+    
+    fetchFilms();
+  }, []);
+
+  return {films, refreshing, loading, onRefresh};
+}

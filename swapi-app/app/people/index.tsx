69032482374801de45247b9d2413937f4a6fd0cc
SoundtrkSamurai <furtimat3@yahoo.com>
2025-07-09 00:26:17 -0700

    refactor: remove unnecessary ScrollView wrapper around FlatList for improved performance

--- swapi-app/app/people/index.tsx
+++ swapi-app/app/people/index.tsx
@@ -3,27 +3,25 @@ import ListEmptyComponent from "@/components/ListEmptyComponent";
 import { COLORS } from "@/constants/colors";
 import { useFetchCharacters } from "@/hooks/useFetchCharacters";
 import React from "react";
-import { FlatList, RefreshControl, ScrollView, StyleSheet } from "react-native";
+import { FlatList, RefreshControl, StyleSheet } from "react-native";
 
 const Characters = () => {
   const { characters, loading, refreshing, onRefresh } = useFetchCharacters();
 
   return (
-    <ScrollView style={styles.container}>
-      <FlatList
-        data={characters}
-        keyExtractor={(item) => item.url.split("/").filter(Boolean).pop() || ""}
-        renderItem={({ item }) => <CharacterItem item={item} />}
-        refreshControl={
-          <RefreshControl
-            onRefresh={onRefresh}
-            refreshing={refreshing}
-            tintColor={COLORS.text}
-          />
-        }
-        ListEmptyComponent={<ListEmptyComponent loading={loading} />}
-      />
-    </ScrollView>
+    <FlatList
+      data={characters}
+      keyExtractor={(item) => item.url.split("/").filter(Boolean).pop() || ""}
+      renderItem={({ item }) => <CharacterItem item={item} />}
+      refreshControl={
+        <RefreshControl
+          onRefresh={onRefresh}
+          refreshing={refreshing}
+          tintColor={COLORS.text}
+        />
+      }
+      ListEmptyComponent={<ListEmptyComponent loading={loading} />}
+    />
   );
 };
 

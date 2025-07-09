a0a0c491bf88e9d460dbc4d50f4aaceed6bc2e90
SoundtrkSamurai <furtimat3@yahoo.com>
2025-07-08 19:08:04 -0700

    feat: add additional AsyncStorage utility functions for merging, clearing, and retrieving keys and items

--- swapi-app/hooks/useAsyncStorage.ts
+++ swapi-app/hooks/useAsyncStorage.ts
@@ -38,12 +38,51 @@ const useAsyncStorage = <T>(key: string) => {
     }
   };
 
+  const mergeItem = async (newvalue: T) => {
+  try {
+    await AsyncStorage.mergeItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error('Error merging item:', error);
+  }
+};
+
+const clear = async () => {
+  try {
+    await AsyncStorage.clear();
+  } catch (error) {
+    console.error('Error clearing AsyncStorage:', error);
+  }
+};
+
+const getAllKeys = async () => {
+  try {
+    return await AsyncStorage.getAllKeys();
+  } catch (error) {
+    console.error('Error getting all keys:', error);
+    return [];
+  }
+};
+
+const getAllItems = async () => {
+  try {
+    const keys = await AsyncStorage.getAllKeys();
+    const items = await AsyncStorage.multiGet(keys);
+    return items.reduce((accumulator: Record<string, T>, [key, value]) => {
+      accumulator[key] = JSON.parse(value as string);
+      return accumulator;
+    }, {} as { [key: string]: any });
+  } catch (error) {
+    console.error('Error getting all items:', error);
+    return {};
+  }
+};
+
   // Fetch data when the component mounts
   useEffect(() => {
     getData();
   }, [getData]);
 
-  return { value, setData, removeData, error };
+  return { value, setData, removeData, error, mergeItem, getAllItems, getAllKeys, clear };
 };
 
 export default useAsyncStorage;
